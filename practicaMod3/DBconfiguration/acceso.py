08fb045ab1ff9ac4b774e26750ffb250ab50e614
Elías Alejandro Ramírez García <39421514+EAeliasalejandro@users.noreply.github.com>
2026-06-09 16:36:46 +0000

    manipulación de datos y tablas

--- practicaMod3/DBconfiguration/acceso.py
+++ practicaMod3/DBconfiguration/acceso.py
@@ -57,14 +57,115 @@ def obtener_datos_usuario(username, password):
     except Exception as e:
         print("Error de consulta a base de datos: ",e)
 
+
+def insertar_usuario(nombre, correo ,telefono, fecha_nacimiento, username, password):
+    conn = conectar_db()
+    if not conn:
+        return
+    try:
+        cursor = conn.cursor()
+
+        #Insertar datos
+        cursor.execute(
+            """
+                INSERT INTO usuarios (nombre, correo, telefono, fecha_nacimiento) 
+                VALUES (%s, %s, %s, %s) RETURNING id_usuario;
+            """, (nombre, correo, telefono, fecha_nacimiento)
+        )
+
+        # Obtener el ide del usuario recien insertado
+        id_usuario = cursor.fetchone()[0]
+
+        #Insertar las credenciales nuevas
+        cursor.execute(
+            """
+                INSERT INTO credenciales (id_usuario, username, password_hash) 
+                VALUES (%s, %s, %s);
+            """, (id_usuario, username, password)
+        )
+
+        #Confirmar los cambios en la base de datos
+        conn.commit()
+        print("Se insertaron los datos correctamente")
+
+    except Exception as e:
+        print("Error al insertar: ", e)
+        conn.rollback()
     
-if __name__=="__main__":
-    print("Inicio de sesión en la base de datos")
+    finally:
+        cursor.close()
+        conn.close()
+
+def actualizar_correo(id_usuario, correoNuevo):
+    conn = conectar_db()
+    if not conn:
+        return
+    try:
+
+        cursor = conn.cursor()
+        #Modificar un correo apartir del id_usuario
+        cursor.execute(
+            "UPDATE usuarios SET correo = %s WHERE id_usuario = %s;", (correoNuevo, id_usuario)
+        )
+        conn.commit()
+        print("Correo actualizado correctamente.")
+
+    except Exception as e:
+        print("Error al modificar: ", e)
+        conn.rollback()    
+    
+    finally:
+        cursor.close()
+        conn.close()
+
+
+def eliminar_usuario(id_usuario):
+    conn = conectar_db()
+    if not conn:
+        return
+    try:
+        cursor = conn.cursor()
         
+        #Eliminación de credencial
+        cursor.execute("DELETE FROM credenciales WHERE id_credencial = %s;",(id_usuario))
+
+        #Eliminar usuario
+        cursor.execute("DELETE FROM usuarios WHERE id_usuario = %s;",(id_usuario))
+
+        conn.commit()
+        print("Usuario eliminado correctamente")
+
+    except Exception as e:
+        print("Error al eliminar: ",e)
+        conn.rollback()
+
+    finally:
+        cursor.close()
+        conn.close()
+
+if __name__=="__main__":
+    #print("Inicio de sesión en la base de datos")
     #Solicitar credenciales del usuario
-    user = input("Ingrese un usuario: ")
-
+    #user = input("Ingrese un usuario: ")
     #Solicitar la contraseña sin que se vea
-    pdw = getpass.getpass("Ingresar contraseña: ")
+    #pdw = getpass.getpass("Ingresar contraseña: ")
+    #obtener_datos_usuario(user, pdw)
 
-    obtener_datos_usuario(user, pdw)
+    #Añadir nuevos usuarios
+    #print("Insertar nuevos usuarios")
+    #nombre = input("Ingresa el nombre del nuevo usuario: ")
+    #correo = input("Ingresa el nuevo correo")
+    #telefono = input("Ingresa el telefono del nuevo usuario: ")
+    #fecha_nacimiento = input("Ingresa la fecha de nacimiento del nuevo usuario: ")
+    #username = input("Ingresa el nuevo usuario: ")
+    #password = input("Ingresa la contraseña del nuevo usuario: ")
+    #insertar_usuario(nombre, correo, telefono, fecha_nacimiento, username, password)
+
+    #Modificar correos
+    #id_usuario = input("Ingresa el id del usuario que quieres modificar: ")
+    #correoNuevo = input("Ingresa el correo nuevo: ")
+    #actualizar_correo(id_usuario,correoNuevo)
+
+    #Elminar usuarios
+    id_usuario = input("Ingresa el id del usuario que quieres eliminar: ")
+    eliminar_usuario(id_usuario)
